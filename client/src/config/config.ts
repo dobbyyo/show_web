fd54ee36a4c3a981666974204c45cfd6d0c67595
dobbyyo <rlaekals9709@gmail.com>
2022-12-11 17:15:01 +0900

    url change

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,3 @@
-const backUrl = process.env.NODE_ENV === 'production' ? 'http://52.78.219.84' : 'http://localhost:3100';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.36.131.161' : 'http://localhost:3100';
 
 export default backUrl;
